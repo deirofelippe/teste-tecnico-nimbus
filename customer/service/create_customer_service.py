b4eeb1ed7f5fc8c79c308b339eae7c596403df28
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-14 23:32:45 -0300

    feat(service): cria service para create customer

---
+++ customer/service/create_customer_service.py
@@ -0,0 +1,59 @@
+import uuid
+from customer.repository.customer_repository import CustomerRepository
+from customer.validator.create_customer_validator import CreateCustomerValidator
+from logger import logger
+
+
+class CreateCustomerService:
+    def __init__(
+        self,
+        customer_repository: CustomerRepository,
+        create_customer_validator: CreateCustomerValidator,
+    ):
+        self.customer_repository = customer_repository
+        self.create_customer_validator = create_customer_validator
+
+    def execute(self, customer_data) -> dict:
+        logger.info("Iniciando o service")
+        logger.debug("Dados iniciais")
+        logger.debug(customer_data)
+
+        result = {"errors": {}}
+
+        data = customer_data["data"]
+        data = data.split(",")
+
+        if len(data) != 4:
+            result["errors"]["others"] = [
+                "Falta campos ou o número de campos foi excedido. Informe o nome, email, telefone e idade."
+            ]
+
+            logger.warning("Falha na validação")
+            logger.warning(result)
+            return result
+
+        customer = {
+            "name": data[0],
+            "email": data[1],
+            "phone": data[2],
+            "age": data[3],
+        }
+
+        logger.info("Iniciando validação")
+        errors = self.create_customer_validator.execute(customer)
+
+        if len(errors) > 0:
+            result["errors"].update(errors)
+
+            logger.warning("Falha na validação")
+            logger.warning(result)
+            return result
+
+        customer["id"] = uuid.uuid4()
+
+        self.customer_repository.create(customer)
+
+        logger.debug("Retorno do service")
+        logger.debug(result)
+
+        return result

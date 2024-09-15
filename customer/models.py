6ad8d768ce6d910cb3c7e164dd893850365a92d1
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-14 23:22:44 -0300

    feat(model): cria model pra customer

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=70)
+    phone = models.CharField(max_length=13)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = "customers"

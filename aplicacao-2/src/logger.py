51a27da5467f4e5a58020c9c4ebd44643b4a6c9a
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-16 16:51:12 -0300

    config(logger): cria logger usando loguru e configura timezone

---
+++ aplicacao-2/src/logger.py
@@ -0,0 +1,17 @@
+import os, time
+from loguru import logger
+from datetime import datetime
+
+os.environ["TZ"] = "America/Sao_Paulo"
+time.tzset()
+
+now = datetime.now().strftime("%Y%m%d")
+format = "{time:YYYY-MM-DD  HH:mm:ss} | {level} | {message} "
+
+logger.remove(0)
+logger.add(
+    f"/app/aplicacao-2/logs/log_{now}.log",
+    rotation="1 days",
+    retention="7 days",
+    format=format,
+)

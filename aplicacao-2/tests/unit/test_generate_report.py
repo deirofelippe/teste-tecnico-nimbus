67340181da7812161cba770ddf3bff43cf529a8b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-16 13:55:56 -0300

    test(unit): adiciona asserts para generate_Report

--- aplicacao-2/tests/unit/test_generate_report.py
+++ aplicacao-2/tests/unit/test_generate_report.py
@@ -1,15 +1,10 @@
-from faker import Faker
-import pytest
-from pytest_mock.plugin import MockerFixture
-from mocks.processed_raw_file import analysis, forecast, header_info
+from tests.mocks.mock_processed_raw_file import analysis, forecast, header_info
 from src.generate_report import generate_report
 
 
-@pytest.fixture
-def setup_data():
-    data = 1
-    yield data
+def test_deve_gerar_o_relatorio():
+    filename = generate_report(
+        header_info=header_info, analysis=analysis, forecast=forecast
+    )
 
-
-def test_deve_gerar_o_relatorio(mocker: MockerFixture, setup_data: int):
-    generate_report(header_info=header_info, analysis=analysis, forecast=forecast)
+    assert "/reports/Relatorio_Meteorologico_" in filename

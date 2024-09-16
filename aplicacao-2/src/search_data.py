ccb8b521e419766ccd180873a13f31dd7a251780
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-16 16:52:04 -0300

    feat: cria etapa de buscar os dados do cliente no banco de dados

---
+++ aplicacao-2/src/search_data.py
@@ -0,0 +1,58 @@
+import mysql.connector
+import traceback
+from src.logger import logger
+
+
+def search_data(phones: list) -> list:
+    logger.info("SEARCHDATA -> Input: ")
+    logger.info(phones)
+    logger.info(
+        "SEARCHDATA -> Iniciando a etapa de busca de customers no banco de dados..."
+    )
+    list_in = list(map(lambda item: "%s", phones))
+    list_in = ",".join(list_in)
+    list_in = f"({list_in})"
+
+    customers = []
+
+    customers = execute_query(list_in, phones)
+
+    customer_tuple_to_dict = lambda customer: {
+        "name": customer[0],
+        "email": customer[1],
+    }
+
+    customers = map(customer_tuple_to_dict, customers)  # type: ignore
+    customers = list(customers)
+
+    logger.info(
+        "SEARCHDATA -> Finalizando a etapa de busca de customers no banco de dados..."
+    )
+    logger.info("SEARCHDATA -> Output:")
+    logger.info(customers)
+    return customers
+
+
+def execute_query(list_in: str, phones: list):
+    try:
+        connection = mysql.connector.connect(
+            host="db", user="root", password="root", database="teste_nimbus"
+        )
+        cursor = connection.cursor()
+        logger.info("SEARCHDATA -> Buscando customers no banco de dados...")
+        cursor.execute(
+            f"SELECT name, email FROM customers WHERE phone IN {list_in}",
+            phones,
+        )
+        customers = cursor.fetchall()
+        logger.info("SEARCHDATA -> Customers buscados")
+        logger.info(customers)
+    except Exception as e:
+        logger.error("SEARCHDATA -> Erro")
+        logger.error(traceback.format_exc())
+        raise e
+    finally:
+        cursor.close()
+        connection.close()
+
+    return customers

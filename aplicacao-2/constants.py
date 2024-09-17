07eb619c5083080b67aa84d4783be58644ebeb96
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-09-16 23:00:11 -0300

    config: cria arquivo de constantes

---
+++ aplicacao-2/constants.py
@@ -0,0 +1,8 @@
+FROM_EMAIL = "teste.from@gmail.com"
+FROM_PASSWORD = "senha"
+NAME_EMAIL = "Teste From"
+SMTP_HOST_TEST = "mailhog"
+SMTP_PORT_TEST = 1025
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 465
+SMTP_TEST = True

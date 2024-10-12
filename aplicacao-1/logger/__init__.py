1f3ee5f6abca02a78c9828397738ec70b56769d6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2024-10-12 03:05:18 -0300

    feat: adiciona log no terminal e em arquivo

--- aplicacao-1/logger/__init__.py
+++ aplicacao-1/logger/__init__.py
@@ -1,6 +1,9 @@
+import sys
 import os, time
 from loguru import logger
 from datetime import datetime
+
+log_type = os.environ.get("LOG_TYPE")
 
 os.environ["TZ"] = "America/Sao_Paulo"
 time.tzset()
@@ -9,9 +12,13 @@ now = datetime.now().strftime("%Y%m%d")
 format = "{time:YYYY-MM-DD  HH:mm:ss} | {level} | {message} "
 
 logger.remove(0)
-logger.add(
-    f"/app/aplicacao-1/logs/log_{now}.log",
-    rotation="1 days",
-    retention="7 days",
-    format=format,
-)
+
+if log_type == "file":
+    logger.add(
+        f"/app/aplicacao-1/logs/log_{now}.log",
+        rotation="1 days",
+        retention="7 days",
+        format=format,
+    )
+else:
+    logger.add(sys.stdout, colorize=True, format=format)
